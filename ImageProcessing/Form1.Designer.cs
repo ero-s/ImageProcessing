5bd0f27ab32250760ceb9307b05c16a5524ea4a1
Austine <austinelomocso@gmail.com>
2025-09-23 16:31:47 +0800

    part2

--- ImageProcessing/Form1.Designer.cs
+++ ImageProcessing/Form1.Designer.cs
@@ -32,20 +32,32 @@ namespace ImageProcessing
             panel1 = new Panel();
             pictureBox1 = new PictureBox();
             panel2 = new Panel();
+            pictureBox2 = new PictureBox();
             pictureBox3 = new PictureBox();
             menuStrip1 = new MenuStrip();
             fileToolStripMenuItem = new ToolStripMenuItem();
             uploadToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
             openFileDialog1 = new OpenFileDialog();
             button1 = new Button();
             button2 = new Button();
             button3 = new Button();
-            saveToolStripMenuItem = new ToolStripMenuItem();
+            button4 = new Button();
+            button5 = new Button();
+            panel3 = new Panel();
+            pictureBox4 = new PictureBox();
+            button6 = new Button();
+            openFileDialog2 = new OpenFileDialog();
+            button7 = new Button();
+            button8 = new Button();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             menuStrip1.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             SuspendLayout();
             // 
             // panel1
@@ -67,11 +79,22 @@ namespace ImageProcessing
             // 
             // panel2
             // 
+            panel2.Controls.Add(pictureBox2);
             panel2.Controls.Add(pictureBox3);
             panel2.Location = new Point(394, 36);
             panel2.Name = "panel2";
             panel2.Size = new Size(399, 345);
             panel2.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Dock = DockStyle.Fill;
+            pictureBox2.Location = new Point(0, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(399, 345);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
@@ -88,7 +111,7 @@ namespace ImageProcessing
             menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(805, 24);
+            menuStrip1.Size = new Size(1225, 24);
             menuStrip1.TabIndex = 2;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -102,9 +125,16 @@ namespace ImageProcessing
             // uploadToolStripMenuItem
             // 
             uploadToolStripMenuItem.Name = "uploadToolStripMenuItem";
-            uploadToolStripMenuItem.Size = new Size(180, 22);
+            uploadToolStripMenuItem.Size = new Size(103, 22);
             uploadToolStripMenuItem.Text = "Open";
             uploadToolStripMenuItem.Click += uploadToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(103, 22);
+            saveToolStripMenuItem.Text = "Save";
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
             // 
             // openFileDialog1
             // 
@@ -112,7 +142,7 @@ namespace ImageProcessing
             // 
             // button1
             // 
-            button1.Location = new Point(12, 415);
+            button1.Location = new Point(12, 462);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 3;
@@ -122,7 +152,7 @@ namespace ImageProcessing
             // 
             // button2
             // 
-            button2.Location = new Point(93, 415);
+            button2.Location = new Point(93, 462);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 5;
@@ -132,7 +162,7 @@ namespace ImageProcessing
             // 
             // button3
             // 
-            button3.Location = new Point(174, 415);
+            button3.Location = new Point(174, 462);
             button3.Name = "button3";
             button3.Size = new Size(115, 23);
             button3.TabIndex = 6;
@@ -140,18 +170,90 @@ namespace ImageProcessing
             button3.UseVisualStyleBackColor = true;
             button3.Click += button3_Click;
             // 
-            // saveToolStripMenuItem
+            // button4
             // 
-            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            saveToolStripMenuItem.Size = new Size(180, 22);
-            saveToolStripMenuItem.Text = "Save";
-            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
+            button4.Location = new Point(295, 462);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 7;
+            button4.Text = "Sepia";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(376, 462);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 8;
+            button5.Text = "Histogram";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(pictureBox4);
+            panel3.Location = new Point(799, 36);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(415, 345);
+            panel3.TabIndex = 9;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Dock = DockStyle.Fill;
+            pictureBox4.Location = new Point(0, 0);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(415, 345);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 0;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(557, 387);
+            button6.Name = "button6";
+            button6.Size = new Size(81, 23);
+            button6.TabIndex = 10;
+            button6.Text = "Load Image";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // openFileDialog2
+            // 
+            openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // button7
+            // 
+            button7.Location = new Point(985, 387);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 23);
+            button7.TabIndex = 11;
+            button7.Text = "Subtract";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(126, 387);
+            button8.Name = "button8";
+            button8.Size = new Size(117, 23);
+            button8.TabIndex = 12;
+            button8.Text = "Load Background";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(805, 450);
+            ClientSize = new Size(1225, 497);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(panel3);
+            Controls.Add(button5);
+            Controls.Add(button4);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
@@ -164,9 +266,12 @@ namespace ImageProcessing
             panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
+            panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -184,5 +289,14 @@ namespace ImageProcessing
         private Button button2;
         private Button button3;
         private ToolStripMenuItem saveToolStripMenuItem;
+        private Button button4;
+        private Button button5;
+        private PictureBox pictureBox2;
+        private Panel panel3;
+        private PictureBox pictureBox4;
+        private Button button6;
+        private OpenFileDialog openFileDialog2;
+        private Button button7;
+        private Button button8;
     }
 }
--- ImageProcessing/Form1.cs
+++ ImageProcessing/Form1.cs
@@ -1,7 +1,11 @@
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace ImageProcessing
 {
     public partial class Form1 : Form
     {
+        Bitmap imageA, imageB;
         public Form1()
         {
             InitializeComponent();
@@ -29,7 +33,7 @@ namespace ImageProcessing
         {
             if (pictureBox1.Image != null)
             {
-                pictureBox3.Image = (Image)pictureBox1.Image.Clone();
+                pictureBox2.Image = (Image)pictureBox1.Image.Clone();
             }
         }
 
@@ -62,7 +66,7 @@ namespace ImageProcessing
                     grayScale.SetPixel(x, y, grayColor);
                 }
             }
-            pictureBox3.Image = grayScale;
+            pictureBox2.Image = grayScale;
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -82,17 +86,17 @@ namespace ImageProcessing
                     inverse.SetPixel(x, y, newColor);
                 }
             }
-            pictureBox3.Image = inverse;
+            pictureBox2.Image = inverse;
         }
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if(pictureBox3.Image != null)
+            if (pictureBox2.Image != null)
             {
                 string filePath = "C:\\Users\\austi\\Downloads\\saved_image.jpg";
                 try
                 {
-                    pictureBox3.Image.Save(filePath, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    pictureBox2.Image.Save(filePath, System.Drawing.Imaging.ImageFormat.Jpeg);
                     MessageBox.Show("Image saved successfully to " + filePath);
                 }
                 catch (Exception ex)
@@ -103,6 +107,121 @@ namespace ImageProcessing
             else
             {
                 MessageBox.Show("No image to save.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null) return;
+            Bitmap source = (Bitmap)pictureBox1.Image;
+            Bitmap sepia = new Bitmap(source.Width, source.Height);
+
+            for (int x = 0; x < source.Width; x++)
+            {
+                for (int y = 0; y < source.Height; y++)
+                {
+                    Color pixelColor = source.GetPixel(x, y);
+
+                    int tr = (int)(0.393 * pixelColor.R + 0.769 * pixelColor.G + 0.189 * pixelColor.B);
+                    int tg = (int)(0.349 * pixelColor.R + 0.686 * pixelColor.G + 0.168 * pixelColor.B);
+                    int tb = (int)(0.272 * pixelColor.R + 0.534 * pixelColor.G + 0.131 * pixelColor.B);
+
+                    tr = Math.Min(255, tr);
+                    tg = Math.Min(255, tg);
+                    tb = Math.Min(255, tb);
+
+                    Color newColor = Color.FromArgb(pixelColor.A, tr, tg, tb);
+                    sepia.SetPixel(x, y, newColor);
+                }
+            }
+            pictureBox2.Image = sepia;
+        }
+        private void MakeHistogram(Bitmap bmp, PictureBox pictureBox)
+        {
+            int[] histogram = new int[256];
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color c = bmp.GetPixel(x, y);
+                    int gray = (int)(0.299 * c.R + 0.587 * c.G + 0.114 * c.B);
+                    histogram[gray]++;
+                }
+            }
+
+            int max = histogram.Max();
+            Bitmap histImage = new Bitmap(256, 100);
+            using (Graphics g = Graphics.FromImage(histImage))
+            {
+                g.Clear(Color.White);
+                for (int i = 0; i < 256; i++)
+                {
+                    int barHeight = (int)(histogram[i] * 100.0 / max);
+                    g.DrawLine(Pens.Black, i, 100, i, 100 - barHeight);
+                }
+            }
+            pictureBox.Image = histImage;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null) return;
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            MakeHistogram(bmp, pictureBox4);
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.FileName = "";
+            openFileDialog2.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+            openFileDialog2.ShowDialog();
+            if (!string.IsNullOrEmpty(openFileDialog2.FileName))
+            {
+                Image img = Image.FromFile(openFileDialog2.FileName);
+                pictureBox2.Image = img;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            imageB = (Bitmap)pictureBox1.Image;
+            imageA = (Bitmap)pictureBox2.Image;
+            Color myGreen = Color.FromArgb(0, 255, 0);
+            int greyGreen = (int)(myGreen.R + myGreen.G + myGreen.B) / 3;
+            int threshold = 3;
+
+            for (int x = 0; x < imageA.Width; x++)
+            {
+                for (int y = 0; y < imageA.Height; y++)
+                {
+                    Color pixelColor = imageA.GetPixel(x, y);
+                    int greyPixel = (int)(pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                    if (Math.Abs(greyPixel - greyGreen) < threshold)
+                    {
+                        Color bgColor = imageB.GetPixel(x, y);
+                        imageA.SetPixel(x, y, bgColor);
+                    }
+                }
+            }
+            pictureBox4.Image = imageA;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+            openFileDialog1.ShowDialog();
+            if (!string.IsNullOrEmpty(openFileDialog2.FileName))
+            {
+                Image img = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.Image = img;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             }
         }
     }
